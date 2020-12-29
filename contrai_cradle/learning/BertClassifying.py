ec2a884842be30c7e2a0cdfcf69491c6b0fc2209
jintaolob <jintaolong@brookes.ac.uk>
2020-12-29 15:55:59 +0000

    correct data prep pipeline glitches

--- contrai_cradle/learning/BertClassifying.py
+++ contrai_cradle/learning/BertClassifying.py
@@ -162,7 +162,8 @@ class BertClassifying(LogisticClassifying):
             self._last_hidden_states = self._model(input_ids.long())
 
     def _comply_input_format(self):
-        features = self._last_hidden_states.to_dict(orient='records')
+        features = self._last_hidden_states[0][:,0,:]
+        features = features.to_dict(orient='records')
         labels = self._df[1]
         output = []
         for count in range(len(features)):
