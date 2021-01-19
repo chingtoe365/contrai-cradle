185fcced9f07b4844cd69e43fbda6ba5afc4495b
jintaolob <jintaolong@brookes.ac.uk>
2021-01-19 13:31:36 +0000

    Bert base sequence classifier

--- contrai_cradle/learning/DistilBertClassifying.py
+++ contrai_cradle/learning/DistilBertClassifying.py
@@ -11,12 +11,12 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 
 from contrai_cradle.db.db_connector import CONN
-# from contrai_cradle.learning.LogisticClassifying import LogisticClassifying
-from contrai_cradle.abstracts.MLAbstract import MLAbstract
+from contrai_cradle.learning.LogisticClassifying import LogisticClassifying
+# from contrai_cradle.abstracts.MLAbstract import MLAbstract
 from contrai_cradle.config.constants import TRAINING_INGREDIENT_PATH
 
 
-class BertClassifying(MLAbstract):
+class DistilBertClassifying(LogisticClassifying):
     """
     DistilBERT & Logistic Regression 
 
@@ -65,31 +65,38 @@ class BertClassifying(MLAbstract):
         ### MODEL SPECIFIC OPERATIONS ###
         self._load_data()
         self._import_bert_model_and_tokenizer()
-        self._tokenize_and_train()
-        # token_lists = self._tokenization()
-        # padded = self._pad_and_trim(token_lists)
-        # self._process_with_distil_bert(padded)
+        token_lists = self._tokenization()
+        padded = self._pad_and_trim(token_lists)
+        self._process_with_distil_bert(padded)
         # assign samples to _observation as data hub 
         # following class tradition
-        # self._observation = self._comply_input_format()
+        self._observation = self._comply_input_format()
         ### MODEL SPECIFIC OPERATIONS ###
 
         self._training_start = datetime.datetime.strftime(
             datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
-        # self.cross_validation()
+        self.cross_validation()
         # self._preprocessing_start = prev_output.preprocessing_start
         # self._preprocessing_end = prev_output.preprocessing_end
         self._training_end = datetime.datetime.strftime(
             datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
-        # self._save_model()
+        self._save_model()
         # assiging dummy value to testing time records to avoid errors
         self._testing_start = datetime.datetime.strftime(
             datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
         self._testing_end = datetime.datetime.strftime(
             datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
         # TODO: plot figures
-        # self.vis_path = ""
-        # self._save_result()
+        self.vis_path = ""
+        self._save_result()
+
+    # def _train_and_test(self):
+    #     lr_clf = LogisticRegression(
+    #         C=self._param_c,
+    #         max_iter=self._param_max_iter
+    #     )
+    #     lr_clf.fit(self._train_features, self._train_labels)
+    #     lr_clf.score(self._test_features, self._test_labels)
 
     def _load_data(self):
         """
@@ -113,7 +120,7 @@ class BertClassifying(MLAbstract):
 
     def _import_bert_model_and_tokenizer(self):
         model_class, tokenizer_class, pretrained_weights = (
-            ppb.BertForSequenceClassification, ppb.BertTokenizer, 'bert-base-uncased'
+            ppb.DistilBertModel, ppb.DistilBertTokenizer, 'distilbert-base-uncased'
         )
 
         ## Want BERT instead of distilBERT? Uncomment the following line:
@@ -125,46 +132,36 @@ class BertClassifying(MLAbstract):
         self._tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
         self._model = model_class.from_pretrained(pretrained_weights)
 
-    def _tokenize_and_train(self):
-        texts = self._df[0].tolist()
-        inputs = tokenizer(texts, return_tensors="pt", padding=True)
-        labels = torch.tensor(self._df[1].tolist()).unsqueeze(0)
-        outputs = self._model(**inputs, labels=labels)
-        print("Model Loss")
-        print(outputs.loss)
-        print("Model Logits")
-        print(outputs.logits)
+    def _tokenization(self):
+        return self._df[0].apply(
+            (
+                lambda x: self._tokenizer.encode(x, add_special_tokens=True)
+            )
+        )
 
-    # def _tokenization(self):
-    #     return self._df[0].apply(
-    #         (
-    #             lambda x: self._tokenizer.encode(x, add_special_tokens=True)
-    #         )
-    #     )
+    def _pad_and_trim(self, token_lists):
+        token_lists = [
+            seq_val for seq_key, seq_val in token_lists.to_dict().items()
+        ]
+        df = pd.DataFrame(token_lists)
+        df = df.iloc[:,:self._max_length]
+        # replace nulls with token id 0 - pad with 0 for shorter sequences
+        df = df.replace(np.nan, 0)
+        return np.array(df)
 
-    # def _pad_and_trim(self, token_lists):
-    #     token_lists = [
-    #         seq_val for seq_key, seq_val in token_lists.to_dict().items()
-    #     ]
-    #     df = pd.DataFrame(token_lists)
-    #     df = df.iloc[:,:self._max_length]
-    #     # replace nulls with token id 0 - pad with 0 for shorter sequences
-    #     df = df.replace(np.nan, 0)
-    #     return np.array(df)
+    def _process_with_distil_bert(self, padded):
+        input_ids = torch.tensor(padded)
 
-    # def _process_with_distil_bert(self, padded):
-    #     input_ids = torch.tensor(padded)
+        with torch.no_grad():
+            self._last_hidden_states = self._model(input_ids.long())
 
-    #     with torch.no_grad():
-    #         self._last_hidden_states = self._model(input_ids.long())
-
-    # def _comply_input_format(self):
-    #     features = self._last_hidden_states[0][:,0,:].numpy()
-    #     features = pd.DataFrame(features).to_dict(orient='records')
-    #     labels = self._df[1]
-    #     output = []
-    #     for count in range(len(features)):
-    #         output.append([features[count], labels[count]])
-    #     del self._df
-    #     del self._last_hidden_states
-    #     return output
+    def _comply_input_format(self):
+        features = self._last_hidden_states[0][:,0,:].numpy()
+        features = pd.DataFrame(features).to_dict(orient='records')
+        labels = self._df[1]
+        output = []
+        for count in range(len(features)):
+            output.append([features[count], labels[count]])
+        del self._df
+        del self._last_hidden_states
+        return output
